7a4e075a8d98778cded52376ea11a68c8e3672b0
Kalpa Welivitigoda <callkalpa@gmail.com>
2016-08-08 23:19:06 +0530

    Change the primary unit time for the time varying stat based on the time range

--- features/org.wso2.carbon.analytics.http.feature/src/main/http-analytics/api/modules/time-varying-stat.js
+++ features/org.wso2.carbon.analytics.http.feature/src/main/http-analytics/api/modules/time-varying-stat.js
@@ -22,13 +22,14 @@ var helper = require('as-data-util.js');
 
 // This map holds the field name, operation and alias and for request count, response time and error count
 var parameterMapping = {
-    'request': ['averageRequestCount', 'AVG', 'AVG_averageRequestCount'],
+    'request': ['averageRequestCount', 'SUM', 'SUM_averageRequestCount'],
     'response': ['averageResponseTime', 'AVG', 'AVG_averageResponseTime'],
     'error': ['httpErrorCount','SUM', 'AVG_httpErrorCount']
 };
 
-function getTimeVaryingStatData(conditions, mappedParameters) {
-    results = getAggregateDataFromDAS(REQUEST_SUMMARY_TABLE, conditions, "0", TIME_FACET, [{
+function getTimeVaryingStatData(conditions, mappedParameters, facetField) {
+
+    results = getAggregateDataFromDAS(REQUEST_SUMMARY_TABLE, conditions, "0", facetField, [{
         "fieldName": mappedParameters[0],
         "aggregate": mappedParameters[1],
         "alias": mappedParameters[2]
@@ -38,18 +39,50 @@ function getTimeVaryingStatData(conditions, mappedParameters) {
     return results;
 }
 
-function getTimeVaryingStat(conditions, type, color) {
+function getTimeVaryingStat(conditions, type, startTime, endTime, color) {
     var dataArray = [];
     var i;
     var row;
     var mappedParameters = parameterMapping[type];
 
-    var results = JSON.parse(getTimeVaryingStatData(conditions, mappedParameters));
+    var timeDifference = endTime - startTime;
+
+    // Decide the time unit for summarization based on the following conditions,
+    // if time difference is more than,
+    //      2 years -> year
+    //      2 months -> month
+    //      2 days -> day
+    //      2 hours -> hour
+    //      else -> minute
+    var facetField = TIME_FACET;
+    if (timeDifference >= 63072000000) {
+        facetField = YEAR_FACET;
+    } else if (timeDifference >= 5184000000) {
+        facetField = MONTH_FACET;
+    } else if (timeDifference >= 172800000) {
+        facetField = DAY_FACET;
+    } else if (timeDifference >= 7200000) {
+        facetField = HOUR_FACET;
+    }
+
+    var results = JSON.parse(getTimeVaryingStatData(conditions, mappedParameters, facetField));
     var chartOptions = {};
+
+    var dateCompletionPostfix = ':00.000Z';
+
+    if (facetField == YEAR_FACET) {
+        dateCompletionPostfix = '-00-00T00:00:00.000Z';
+    } else if (facetField == MONTH_FACET) {
+        dateCompletionPostfix = '-00T00:00:00.000Z';
+    } else if (facetField == DAY_FACET) {
+        dateCompletionPostfix = 'T00:00:00.000Z';
+    } else if (facetField == HOUR_FACET) {
+        dateCompletionPostfix = '00:00.000Z';
+    }
 
     for (i = 0; i < results.length; i++) {
         row = results[i]['values'];
-        var time = new Date(String(row[TIME_FACET][0]).replace(' ', 'T') + ':00.000Z').getTime();
+        var time = new Date(String(row[facetField][0]).replace(' ', 'T') + dateCompletionPostfix).getTime();
         dataArray.push({"time": Number(time).toPrecision(), "value":row[mappedParameters[2]]});
     }
 
